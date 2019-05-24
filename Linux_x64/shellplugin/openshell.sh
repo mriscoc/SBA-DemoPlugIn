79409d4f00cc31f8925e9feced75d8bb03125a04
Miguel A. Risco-Castillo <mriscoc@gmail.com>
2019-05-24 13:16:08 -0500

    Revert repository to only Demo Plugin

--- Linux_x64/shellplugin/openshell.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -z "$1" ]; then
-  gnome-terminal --working-directory=randomdir
-else
-  gnome-terminal --working-directory=$1
-fi
-
--- Windows/cmdplugin/opencmd.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-IF [%1] == [] GOTO nopath
-start cmd.exe /k "cd /d %1"
-GOTO exit
-:nopath
-start cmd.exe /k
-:exit
